699621e723cbe9cc4e02c74a274066175edd4d28
Brian Hocking <bhocking2009@gmail.com>
2026-06-06 15:49:58 -0400

    Add world observer coordinator

---
+++ interface/visualization/world_observer.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "world_snapshot.h"
+#include "terminal_observer.h"
+#include "../../core/events/event_bus.h"
+#include "../../systems/energy/energy_system.h"
+#include "../../systems/entity/entity_system.h"
+#include "../../systems/environment/environment_system.h"
+#include <cstddef>
+#include <cstdint>
+#include <deque>
+#include <string>
+#include <vector>
+
+class WorldObserver {
+public:
+    WorldObserver(EventBus& bus,
+                  const EnergySystem& energySystem,
+                  const EntitySystem& entitySystem,
+                  const EnvironmentSystem& environmentSystem,
+                  std::size_t maxRecentEvents = 8);
+
+    void setTerminalEnabled(bool enabled);
+    void saveJsonLines(const std::string& filePath) const;
+
+    const std::vector<WorldSnapshot>& snapshots() const;
+    const WorldSnapshot* latest() const;
+
+private:
+    EventBus& bus_;
+    const EnergySystem& energySystem_;
+    const EntitySystem& entitySystem_;
+    const EnvironmentSystem& environmentSystem_;
+    std::size_t maxRecentEvents_;
+    bool terminalEnabled_;
+    TerminalObserver terminalObserver_;
+    std::deque<std::string> recentEvents_;
+    std::vector<WorldSnapshot> snapshots_;
+
+    void handleEvent(const SimEvent& event);
+    void capture(uint64_t tick);
+    void rememberEvent(const SimEvent& event);
+    std::string describeEvent(const SimEvent& event) const;
+    std::string entityStateName(EntityState state) const;
+};
