69e25e34b59063865f8c81fdc07b6254d3e417bb
Brian Hocking <bhocking2009@gmail.com>
2026-06-09 14:41:20 -0400

    Add civilization state to application wiring

--- core/app/application.h
+++ core/app/application.h
@@ -7,6 +7,7 @@
 #include "../simulation/simulation_controller.h"
 #include "../influence/player_influence.h"
 #include "../../interface/visualization/world_observer.h"
+#include "../../systems/civilization/civilization_system.h"
 #include <memory>
 #include <string>
 #include <cstdint>
@@ -31,14 +32,18 @@ private:
     std::string          configPath_;
     std::string          snapshotPath_;
     std::string          replayPath_;
+    std::string          settlementName_;
     uint64_t             maxTicks_;
+    double               initialPopulation_;
     bool                 terminalObserverEnabled_;
     PlayerInfluenceMode  influenceMode_;
+    CivicDecision        civicDecision_;
 
-    std::shared_ptr<EnergySystem>      energySys_;
-    std::shared_ptr<InformationSystem> infoSys_;
-    std::shared_ptr<EntitySystem>      entitySys_;
-    std::shared_ptr<EnvironmentSystem> envSys_;
-    std::shared_ptr<InteractionSystem> interactionSys_;
-    std::unique_ptr<WorldObserver>     observer_;
+    std::shared_ptr<EnergySystem>       energySys_;
+    std::shared_ptr<InformationSystem>  infoSys_;
+    std::shared_ptr<EntitySystem>       entitySys_;
+    std::shared_ptr<EnvironmentSystem>  envSys_;
+    std::shared_ptr<CivilizationSystem> civilizationSys_;
+    std::shared_ptr<InteractionSystem>  interactionSys_;
+    std::unique_ptr<WorldObserver>      observer_;
 };
